710eefa7dd267fc387bf3067677f4693a16dc3da
Michael Catanese <michaeljcatanese@gmail.com>
2024-06-08 20:51:05 -0400

    router functional

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,16 +1,18 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from .models import db
+from .routes import init_routes
 from config import SQLALCHEMY_DATABASE_URI
-
-db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    
+    # Configuration and database setup
     app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
     db.init_app(app)
-
-    with app.app_context():
-        from . import routes
-        db.create_all()
-
+    
+    # Initialize routes
+    init_routes(app)
+    
     return app
