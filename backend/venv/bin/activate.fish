bd68c3ede784b24c0f82ec8cf9d8e47113eb35e7
Michael Catanese <michaeljcatanese@gmail.com>
2024-06-09 13:20:01 -0400

    address console dependency warning

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mcatanese/workout-logger-assignment/backend/venv"
+set -gx VIRTUAL_ENV "/home/mcatanese/workout-logger/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
